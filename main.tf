8cf0e65d0d2c94c6dcb94e9e2fecd7d0afb2ca77
Akash Rokade <akashdrokade@gmail.com>
2022-05-23 12:03:17 +0530

    subnet resource added

--- main.tf
+++ main.tf
@@ -139,3 +139,15 @@ Purpose = "Intro to Resource Blocks Lab"
 resource "random_id" "randomness" {
 byte_length = 16
 }
+
+resource "aws_subnet" "variables-subnet" {
+vpc_id = aws_vpc.vpc.id
+cidr_block = "10.0.250.0/24"
+availability_zone = "us-east-1a"
+map_public_ip_on_launch = true
+tags = {
+Name = "sub-variables-us-east-1a"
+Terraform = "true"
+}
+}
+
